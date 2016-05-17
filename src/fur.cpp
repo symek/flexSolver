f7b561958f337093fbcab4cb7cc706f5f07240f6
Szymon Kapeniak <szymon.kapeniak@gmail.com>
2016-05-17 20:00:44 +0200

    Fix for wron vertex offsets causing crash in some cases.

--- src/fur.cpp
+++ src/fur.cpp
@@ -40,28 +40,23 @@ void copyPointAttribs(const GU_Detail &gdp, const bool roots,
 
     GA_Offset ptoff;
     int rootValue = 1;
-    GA_FOR_ALL_PTOFF(&gdp, ptoff)
-    {
+    GA_FOR_ALL_PTOFF(&gdp, ptoff) {
         if (root_valid && roots) {
             rootValue = root_handle.get(ptoff);
         }
-        const int offset = static_cast<const int>(ptoff)*4;
-        if (rootValue)
-        {
+        const int offset = static_cast<const int>(ptoff);
+        if (rootValue) {
             const UT_Vector3 pos = gdp.getPos3(ptoff);
-            particles_ptr[offset]   = pos.x();
-            particles_ptr[offset+1] = pos.y();
-            particles_ptr[offset+2] = pos.z();
-            particles_ptr[offset+3] = 1.0;
+            particles_ptr[4*offset]   = pos.x();
+            particles_ptr[4*offset+1] = pos.y();
+            particles_ptr[4*offset+2] = pos.z();
+            particles_ptr[4*offset+3] = .1;
         }
-
-        
-        if (vel_valid)
-        {
-            const UT_Vector3 vel     = vel_handle.get(ptoff);
-            velocities_ptr[offset]   = vel.x();
-            velocities_ptr[offset+1] = vel.y();
-            velocities_ptr[offset+2] = vel.z();
+        if (vel_valid) {
+            const UT_Vector3 vel    = vel_handle.get(ptoff);
+            velocities_ptr[3*offset]   += vel.x(); // We better initialize velocities...
+            velocities_ptr[3*offset+1] += vel.y();
+            velocities_ptr[3*offset+2] += vel.z();
         }
     }  
 }
@@ -77,8 +72,7 @@ void copySpringAttribs(const GU_Detail &gdp,  int   *springIndices,
         const GEO_Primitive *prim = gdp.getGEOPrimitive(*it);
         const GA_Range vertices = prim->getVertexRange();
         GA_Offset vi;
-        for (vi = GA_Offset(1); vi < vertices.getEntries(); ++vi)
-        {           
+        for (vi = GA_Offset(1); vi < vertices.getEntries(); ++vi) {           
             const GA_Offset vtx1 = prim->getVertexOffset(vi-1);
             const GA_Offset vtx2 = prim->getVertexOffset(vi);
             springIndices[2*springCounter]     = static_cast<int>(vtx1);
@@ -98,6 +92,37 @@ void copySpringAttribs(const GU_Detail &gdp,  int   *springIndices,
         }
     }
 
+}
+
+void copyMesh(const GU_Detail &gdp, float *points, int *vertices, \
+            int &npoints, int &npoly, float *lower, float *upper)
+{
+    UT_BoundingBox bbox(0,0,0,1,1,1);
+    if (gdp.getBBox(&bbox)) {
+        lower[0] = bbox.xmin(); lower[1] = bbox.ymin(); 
+        lower[2] = bbox.zmin(); upper[0] = bbox.xmin(); 
+        upper[1] = bbox.ymin(); upper[2] = bbox.zmin();
+    }
+    GA_Offset ptoff;
+    GA_FOR_ALL_PTOFF(&gdp, ptoff) {
+        const UT_Vector3 pos  = gdp.getPos3(ptoff);
+        const int offset = static_cast<int>(ptoff);
+        points[3*offset]   = pos.x(); 
+        points[3*offset+1] = pos.y(); 
+        points[3*offset+2] = pos.z(); 
+    }
+    npoints = gdp.getNumPoints();
+    npoly   = gdp.getNumPrimitives();  
+    int vertex_index = 0;
+    for (GA_Iterator it(gdp.getPrimitiveRange()); !it.atEnd(); ++it) {
+        const GEO_Primitive *prim = gdp.getGEOPrimitive(*it);
+        GA_Primitive::const_iterator vt;
+        for (prim->beginVertex(vt); !vt.atEnd(); ++vt) {
+            const GA_Offset voff = vt.getVertexOffset();
+            vertices[vertex_index] = static_cast<int>(voff);
+            vertex_index++;
+        }
+    }
 }
 
 uint getNumSprings(const GU_Detail &gdp)
@@ -131,10 +156,13 @@ int main(void)
 
 
     GU_Detail gdp;
+    GU_Detail collisionGeo;
     gdp.load("./fur.bgeo");
+    collisionGeo.load("./sphere.bgeo");
+    collisionGeo.convex(); // only triangles
     std::cout << "Setting max particles: " << gdp.getNumPoints() <<std::endl;
 
-    const int maxParticles = gdp.getNumPoints();
+    const int maxParticles = gdp.getNumPoints() + collisionGeo.getNumPoints();
     const int maxDiffuse = 0;
 
     FlexSolver* solver = flexCreateSolver(maxParticles, maxDiffuse);
@@ -152,13 +180,17 @@ int main(void)
     flexSetParams(solver, &parms);
 
     // alloc CUDA pinned host memory to allow asynchronous memory transfers
-    Vec4*  particles  = reinterpret_cast<Vec4*>(flexAlloc(maxParticles*sizeof(Vec4)));
-    Vec3*  velocities = reinterpret_cast<Vec3*>(flexAlloc(maxParticles*sizeof(Vec3)));
+    Vec4*  particles      = reinterpret_cast<Vec4*>(flexAlloc(maxParticles*sizeof(Vec4)));
+    Vec3*  velocities     = reinterpret_cast<Vec3*>(flexAlloc(maxParticles*sizeof(Vec3)));
     float* particles_ptr  = reinterpret_cast<float*>(particles);
     float* velocities_ptr = reinterpret_cast<float*>(velocities);
-    int*   actives        =  reinterpret_cast<int*>(flexAlloc(sizeof(int)*maxParticles));
-    for (int i=0; i < maxParticles; ++i)
+    int*   actives        = reinterpret_cast<int*>(flexAlloc(sizeof(int)*maxParticles));
+    for (int i=0; i < maxParticles; ++i) {
         actives[i] = i;
+        velocities_ptr[3*i]   = 0.0;
+        velocities_ptr[3*i+1] = 0.0;
+        velocities_ptr[3*i+2] = 0.0;
+    }
 
     // set initial particle data
     copyPointAttribs(gdp, false, particles_ptr, velocities_ptr);
@@ -166,6 +198,7 @@ int main(void)
 
     // Springs setup:
     uint  numSprings     = getNumSprings(gdp);
+    std::cout << "Springs created      : " <<  numSprings <<std::endl;
     int   *springIndices = reinterpret_cast<int*>(flexAlloc(sizeof(int)*2*numSprings));
     float *springLengths = reinterpret_cast<float*>(flexAlloc(sizeof(float)*numSprings));
     float *springCoefficients =reinterpret_cast<float*>(flexAlloc(sizeof(float)*numSprings));
@@ -176,6 +209,56 @@ int main(void)
     flexSetVelocities(solver, velocities_ptr, maxParticles, eFlexMemoryHostAsync);
     flexSetSprings(solver, springIndices, springLengths, springCoefficients, \
                 numSprings, eFlexMemoryHostAsync);
+
+
+    /*------------- Collision objects ------------- */
+
+    // std::vector<float> meshPoints;
+    // std::vector<int>   meshIndices;
+    // int npoints = 0; int npoly = 0;
+    // float lower[3]; float upper[3];
+    // meshPoints.resize(collisionGeo.getNumPoints()*3);
+    // meshIndices.resize(collisionGeo.getNumPrimitives()*3);
+    // copyMesh(collisionGeo, &meshPoints[0], &meshIndices[0], npoints, \
+    //     npoly, &lower[0], &upper[0]);
+
+    // FlexTriangleMesh *mesh = flexCreateTriangleMesh();
+    // FlexCollisionTriangleMesh colllisionMesh;
+    // colllisionMesh.mMesh = mesh;
+    // colllisionMesh.mScale = 1.0f;
+    // FlexCollisionGeometry collisionGeometry;
+    // collisionGeometry.mTriMesh = colllisionMesh;
+
+    // flexUpdateTriangleMesh(mesh, &meshPoints[0], &meshIndices[0], npoints, \
+    //     npoly, &lower[0], &upper[0], eFlexMemoryHostAsync);
+    
+
+    
+    // // FlexCollisionSphere sphere;
+    // // sphere.mRadius = 0.5f;
+    // // FlexCollisionGeometry collider;
+    // // collider.mSphere = sphere;
+
+
+    // FlexCollisionGeometry collisionGeometries[1];
+    // collisionGeometries[0] = collisionGeometry;
+
+    // const int numGeometryEntries = 1; 
+    // float shapeAabbMaxs[4] = {0.5, 2.5, 0.5, 0};
+    // float shapeAabbMins[4] = {-0.5, 1.5, -0.5, 0};
+    // int shapeOffsets[1]; shapeOffsets[0] = 1;
+    // float shapePositions[4] = {0,2,0,0};
+    // float shapeRotations[4] = {1,0,0,0};
+    // float shapePrevPositions[4] = {0,2,0,0};
+    // float shapePrevRotations[4] = {1,0,0,0};
+    // int shapeFlags[1]; 
+    // shapeFlags[0] = flexMakeShapeFlags(eFlexShapeTriangleMesh, false);
+    // int numShapes = 1;
+    // flexSetShapes(solver, collisionGeometries, numGeometryEntries, shapeAabbMins, shapeAabbMaxs, 
+    //     shapeOffsets, shapePositions, shapeRotations, shapePrevPositions, shapePrevRotations, \
+    //         shapeFlags, numShapes, eFlexMemoryHostAsync);
+
+   
 
   
     GU_Detail source;
@@ -189,8 +272,14 @@ int main(void)
         // update positions, apply custom force fields, etc
         // ModifyParticles(particles, velocities);
         copyPointAttribs(source, true, particles_ptr, velocities_ptr);
+        // shapePositions[0]     = SYSsin(counter/24.0);
+        // shapePrevPositions[0] = SYSsin(counter-1/24.0);
 
         // update GPU data asynchronously
+
+        // flexSetShapes(solver, geometry, numGeometryEntries, shapeAabbMins, shapeAabbMaxs, 
+        // shapeOffsets, shapePositions, shapeRotations, shapePrevPositions,\
+        // shapePrevRotations, shapeFlags, numShapes, eFlexMemoryHostAsync);
     
         flexSetParticles(solver, particles_ptr, maxParticles, eFlexMemoryHostAsync);
         flexSetVelocities(solver, velocities_ptr, maxParticles, eFlexMemoryHostAsync);
@@ -210,8 +299,7 @@ int main(void)
         flexWaitFence();
 
         GA_Offset ptoff;
-        GA_FOR_ALL_PTOFF(&gdp, ptoff)
-        {
+        GA_FOR_ALL_PTOFF(&gdp, ptoff) {
             const int offset = static_cast<int>(ptoff)*4;
             float *point     = &particles_ptr[offset];
             const UT_Vector3 pos(point[0], point[1], point[2]);
@@ -234,7 +322,7 @@ int main(void)
     flexFree(springLengths);
     flexFree(springCoefficients);
     // flexFree(phases);
-
+    // flexDestroyTriangleMesh(mesh);
     flexDestroySolver(solver);
     flexShutdown();
 
